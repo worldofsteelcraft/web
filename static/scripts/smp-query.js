ac14f3a4750239d493e257538e0f2f151296997e
dplayz <dp@dplayzgames06.tk>
2022-05-08 19:33:38 +0800

    update script for screenshot template support

--- static/scripts/smp-query.js
+++ static/scripts/smp-query.js
@@ -6,10 +6,12 @@ jQuery(document).ready(function( $ ){
 		$('#mapname').text(data.LevelName); 
 		if (data.Online == true) {
 			$('#isonline').append('<img src="/images/serveronline.png" width="12px"><span>Online</span>');
-		  }
+			}
 		else {
 			$('#isonline').append('<img src="/images/serveroffline.png" width="12px"><span>Offline</span>');
 			$('#isofflinecss').append('<style>tr.ping-disable-when-offline{display:none;}</style>');
+			$('#isoffline-sstemplate-css').append('<style>.serverstatus-sstemplate-content{display:none;}</style>');
+			$('#isoffline-sstemplate-text').append('<p><b>Server is offline, please check for maintenance notice.</b></p>');
 		 };
 		$('#playercount').text(data.Players +'/'+ data.MaxPlayers );
 		$('#motd').text(data.ServerName);

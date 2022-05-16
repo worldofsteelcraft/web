06a0d6a6b9df73cb7e95ae246abda6140ae1458b
dplayz <dp@dplayzgames06.tk>
2022-05-16 22:45:05 +0800

    several changes, please check description
    + Adds autoreload (references https://github.com/worldofsteelcraft/woscweb/issues/55)
    + Adds console log for successful fetch
    + Adds dynamic display and hide of some elements
    +/ Accessibility between server and playergraph data

--- static/scripts/smp-query.js
+++ static/scripts/smp-query.js
@@ -1,21 +1,31 @@
 /* Attention to admins/server management: Change the lines with "Here" comments with their corresponding port or IP when migration between hosts take place*/
-jQuery(document).ready(function( $ ){
-	$.getJSON('https://api.bedrockinfo.com/v2/status?server=smp.worldofsteelcraft.tk&port=25576', function(data) {  /* Here */  
-		$('#hostname').text(data.HostName);
-		$('#port').text(data.IPv4Port);
-		$('#version').text(data.Version);
-		$('#mapname').text(data.LevelName); 
-		if (data.Online == true) {
-			$('#isonline').append('<img src="/images/serveronline.png" width="12px"><span>Online</span>');
-			}
-		else {
-			$('#isonline').append('<img src="/images/serveroffline.png" width="12px"><span>Offline</span>');
-			$('#isofflinecss').append('<style>tr.ping-disable-when-offline{display:none;}</style>');
-			$('#isoffline-sstemplate-css').append('<style>.serverstatus-sstemplate-content{display:none;}</style>');
-			$('#isoffline-sstemplate-text').append('<p><b>Server is offline, please check for maintenance notice.</b></p>');
-		 };
-		$('#playercount').text(data.Players +'/'+ data.MaxPlayers );
-		$('#motd').text(data.ServerName);
-	});
-	$('#playergraph').attr('src','https://bedrockinfo.com/data/smp.worldofsteelcraft.tk:25576');  /* Here */
+jQuery(document).ready(function ( $ ){
+	setTimeout(myTimer, 1);
+	function myTimer() {
+		$('#playergraph').attr('src','https://bedrockinfo.com/data/smp.worldofsteelcraft.tk:25576');  /* Here */
+		$.getJSON('https://api.bedrockinfo.com/v2/status?server=smp.worldofsteelcraft.tk&port=25576', function(data) { 
+			$('#isonline').empty();
+			$('#isoffline-sstemplate-css').empty();
+			$('#isoffline-sstemplate-text').empty();
+			$('#isofflinecss').empty();
+			$('#hostname').text(data.HostName);
+			$('#port').text(data.IPv4Port);
+			$('#version').text(data.Version);
+			$('#mapname').text(data.LevelName); 
+			if (data.Online == true) {
+				$('#isonline').append('<img src="/images/serveronline.png" width="12px"><span>Online</span>');
+				}	
+			else {
+				$('#isonline').append('<img src="/images/serveroffline.png" width="12px"><span>Offline</span>');
+				$('#isofflinecss').append('<style>tr.ping-disable-when-offline{display:none;}</style>');
+				$('#isoffline-sstemplate-css').append('<style>.serverstatus-sstemplate-content{display:none;}</style>');
+				$('#isoffline-sstemplate-text').append('<p><b>Server is offline, please check for maintenance notice.</b></p>');
+			 };
+			$('#playercount').text(data.Players +'/'+ data.MaxPlayers );
+			$('#motd').text(data.ServerName);
+			console.log("Data Fetched from API Successfully");
+			clearTimeout(myTimer);
+		});
+	}
+	setInterval(myTimer, 300000);
 });

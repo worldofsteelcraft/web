f51e0b647ad159ee57d3b05a4a58cfd473a202de
dplayz <dp@dplayzgames06.tk>
2022-10-28 14:21:21 +0800

    initial support

--- .tina/config.ts
+++ .tina/config.ts
@@ -1,13 +1,12 @@
 
 import { defineStaticConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineStaticConfig({
   branch,
-  clientId: null,   // Get this from tina.io
-  token: null,      // Get this from tina.io
+  clientId: "82e22a7f-b99f-4479-8c6d-7593f2a83ad0", 
+  token: "efb70c9f48b04dc26d7a2d04129d389e7983b467", 
   build: {
     outputFolder: "admin",
     publicFolder: "static",
@@ -22,7 +21,7 @@ export default defineStaticConfig({
     collections: [
       {
         name: "post",
-        label: "Posts",
+        label: "Announcements",
         path: "content/announcements",
         fields: [
           {
@@ -33,10 +32,64 @@ export default defineStaticConfig({
             required: true,
           },
           {
+            label: 'Date',
+            name: 'date',
+            type: 'datetime',
+            ui: {
+              dateFormat: 'YYYY MM DD',
+              timeFormat: "hh:mm A",
+            }
+          },
+          {
+            label: "Author",
+            name: "author",
+            type: "string",
+            list: true,
+          },
+          {
             type: "rich-text",
             name: "body",
             label: "Body",
             isBody: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+          },
+          {
+            type: "image",
+            name: "customPagePreviewImageURL",
+            label: "Thumbnail/Page Preview",
+          },
+          {
+            type: "boolean",
+            name: "enableComments",
+            label: "Comments",
+          },
+          {
+            type: "boolean",
+            name: "sitemapExclude",
+            label: "Exclude from Sitemap",
+          },
+          {
+            type: "boolean",
+            name: "featured",
+            label: "Featured?",
+          },
+        ],
+      },
+      {
+        name: "blog",
+        label: "Blog",
+        path: "content/Blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
           },
           {
             label: 'Date',
@@ -46,6 +99,193 @@ export default defineStaticConfig({
               dateFormat: 'YYYY MM DD',
               timeFormat: "hh:mm A",
             }
+          },
+          {
+            label: "Author",
+            name: "author",
+            type: "string",
+            list: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "image",
+            name: "customPagePreviewImageURL",
+            label: "Thumbnail/Page Preview",
+          },
+          {
+            type: "boolean",
+            name: "enableComments",
+            label: "Comments",
+          },
+          {
+            type: "boolean",
+            name: "sitemapExclude",
+            label: "Exclude from Sitemap",
+          },
+        ],
+      },
+      {
+        name: "season_download",
+        label: "Season Download",
+        path: "content/season-download",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            label: 'Date',
+            name: 'date',
+            type: 'datetime',
+            ui: {
+              dateFormat: 'YYYY MM DD',
+              timeFormat: "hh:mm A",
+            }
+          },
+          {
+            label: "Author",
+            name: "author",
+            type: "string",
+            list: true,
+          },
+          {
+            type: "string",
+            name: "permalink",
+            label: "Permalink",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+          },
+          {
+            type: "image",
+            name: "customPagePreviewImageURL",
+            label: "Thumbnail/Page Preview",
+          },
+          {
+            type: "boolean",
+            name: "enableComments",
+            label: "Comments",
+          },
+          {
+            type: "boolean",
+            name: "sitemapExclude",
+            label: "Exclude from Sitemap",
+          },
+          {
+            type: "boolean",
+            name: "featured",
+            label: "Featured?",
+          },
+        ],
+      },
+      {
+        name: "ban",
+        label: "Member Ban Notice",
+        path: "content/ban",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            label: 'Date',
+            name: 'date',
+            type: 'datetime',
+            ui: {
+              dateFormat: 'YYYY MM DD',
+              timeFormat: "hh:mm A",
+            }
+          },
+          {
+            label: "Author",
+            name: "author",
+            type: "string",
+            list: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+          },
+          {
+            type: "image",
+            name: "customPagePreviewImageURL",
+            label: "Thumbnail/Page Preview",
+          },
+          {
+            type: "boolean",
+            name: "enableComments",
+            label: "Comments",
+          },
+          {
+            type: "boolean",
+            name: "sitemapExclude",
+            label: "Exclude from Sitemap",
+          },
+          {
+            type: "boolean",
+            name: "featured",
+            label: "Featured?",
+          },
+        ],
+      },
+      {
+        name: "rules",
+        label: "Rules",
+        path: "content/rules",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            label: 'Date',
+            name: 'date',
+            type: 'datetime',
+            ui: {
+              dateFormat: 'YYYY MM DD',
+              timeFormat: "hh:mm A",
+            }
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
           },
         ],
       },
